fc990de3619ec885ed1a040ed91eb94b52201172
Justin Hoffman <jmh120804@gmail.com>
2026-04-27 16:12:31 -0500

    Add: minimum-stack - submission-1

---
+++ Data Structures & Algorithms/minimum-stack/submission-1.py
@@ -0,0 +1,26 @@
+class MinStack:
+
+    def __init__(self):
+        self.s = []
+        self.m = []
+
+    def push(self, val: int) -> None:
+        self.s.append(val)
+        if not self.m:
+            self.m.append(val)
+            return
+        
+        if self.m[-1] > val:
+            self.m.append(val)
+        else:
+            self.m.append(self.m[-1])
+
+    def pop(self) -> None:
+        self.s.pop()
+        self.m.pop()
+
+    def top(self) -> int:
+        return self.s[-1]
+
+    def getMin(self) -> int:
+        return self.m[-1]
